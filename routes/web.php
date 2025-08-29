579a3d765fad7a37025fd1641f3418028c3ccbd8
Ariq Farhan <ariqalthaf445@gmail.com>
2025-08-29 21:31:38 +0700

    feat: add broadcasting channel for user authentication and update route structure

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DisplayController;
+use App\Http\Controllers\KioskController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,11 +9,16 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/display-queue', [DisplayController::class, 'index'])->name('display-queue');
+Route::get('/kiosk', [KioskController::class, 'index'])->name('kiosk');
+Route::post('/kiosk', [KioskController::class, 'store'])->name('kiosk.store');
+
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
