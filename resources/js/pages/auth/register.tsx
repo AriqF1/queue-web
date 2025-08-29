681479c2d14cd4bbc4e08986d157f19d2e62f814
Ariq Farhan <ariqalthaf445@gmail.com>
2025-08-29 19:13:37 +0700

    fix: add cursor pointer to submit buttons in login and register forms

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -80,7 +80,7 @@ export default function Register() {
                                 <InputError message={errors.password_confirmation} />
                             </div>
 
-                            <Button type="submit" className="mt-2 w-full" tabIndex={5}>
+                            <Button type="submit" className="mt-2 w-full cursor-pointer" tabIndex={5}>
                                 {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
                                 Create account
                             </Button>
